8ce28b89741f0e5e97eb66f6499391a3acbe2995
EchoHackCmd <EchoHackCmd@gmail.com>
2021-06-21 07:43:10 +0100

    Update BlockSource.h

--- Cardinal/Memory/SDK/Classes/BlockSource.h
+++ Cardinal/Memory/SDK/Classes/BlockSource.h
@@ -14,7 +14,7 @@ class BlockSource {
 public:
 	Block* getBlock(Vec3i blockPos) {
 		using GetBlock = Block * (__thiscall*)(BlockSource*, Vec3i*);
-		static GetBlock _GetBlock = reinterpret_cast<GetBlock>(Utils::FindSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 2A 3B C8 7C 67 0F BF 47 28 3B C8"));
+		static GetBlock _GetBlock = reinterpret_cast<GetBlock>(Utils::FindSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B ? 8B 4A ? 0F BF 47 ? 3B C8 0F 8C"));
 		return _GetBlock(this, &blockPos);
 	}
 
